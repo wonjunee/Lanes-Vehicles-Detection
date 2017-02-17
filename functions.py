8c977734baac0fdcf2697690039d729279196d1a
WONJUN LEE <mymamyma@gmail.com>
2017-02-17 00:43:21 -0500

    feat: added a unwarp function corners_unwarp function will create data for perspective transformation

--- functions.py
+++ functions.py
@@ -8,3 +8,31 @@ def cal_undistort(img, objpoints, imgpoints):
     ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints,imgpoints,img.shape[:2][::-1],None,None)
     undist = cv2.undistort(img, mtx, dist, None, mtx)
     return undist
+
+# Define a function that takes an image, number of x and y points, 
+# camera matrix and distortion coefficients
+def corners_unwarp(img, nx, ny, mtx, dist):
+    # Use the OpenCV undistort() function to remove distortion
+    undist = cv2.undistort(img, mtx, dist, None, mtx)
+    # Grab the image shape
+    img_size = (img.shape[1], img.shape[0])
+    # For source points I'm grabbing the outer four detected corners
+    src = np.float32(
+	    [[(img_size[0] / 2) - 55, img_size[1] / 2 + 100],
+	    [((img_size[0] / 6) - 10), img_size[1]],
+	    [(img_size[0] * 5 / 6) + 60, img_size[1]],
+	    [(img_size[0] / 2 + 55), img_size[1] / 2 + 100]])
+    # For destination points, I'm arbitrarily choosing some points to be
+    # a nice fit for displaying our warped result 
+	dst = np.float32(
+	    [[(img_size[0] / 4), 0],
+	    [(img_size[0] / 4), img_size[1]],
+	    [(img_size[0] * 3 / 4), img_size[1]],
+	    [(img_size[0] * 3 / 4), 0]])
+    # Given src and dst points, calculate the perspective transform matrix
+    M = cv2.getPerspectiveTransform(src, dst)
+    Minv = cv2.getPerspectiveTransform(dst, src)
+    # Warp the image using OpenCV warpPerspective()
+    warped = cv2.warpPerspective(undist, M, img_size)
+    # Return the resulting image and matrix
+    return warped, M, Minv
