c8d4ab345cd199ecc801dd45e79a6f62c022914e
WONJUN LEE <mymamyma@gmail.com>
2017-02-17 19:41:25 -0500

    fix: deleted the jupyter code Deleted the jupyter notebook code at the end.

--- find_lanes.py
+++ find_lanes.py
@@ -61,5 +61,4 @@ def pipeline(img):
 # right_lane = Line()
 white_output = 'white.mp4'
 clip1 = VideoFileClip("project_video.mp4")
-white_clip = clip1.fl_image(process_image) #NOTE: this function expects color images!!
-%time white_clip.write_videofile(white_output, audio=False)
+white_clip = clip1.fl_image(pipeline) #NOTE: this function expects color images!!
